18b057e9c9907589c6f7f9e9c632965fd210cc63
mkdirprince <mkdirprince@github.com>
2023-09-10 10:49:40 +0000

    add gitignore and modify controls

--- snakeControls.js
+++ snakeControls.js
@@ -1,22 +1,24 @@
-// declare and assign default input direction
+// declare and assign default input direction and last direction
 let inputDirection = {x: 0, y: 1}
+let lastDirection = {x:0, y: 1}
 
 
 // callback function for controls
+// the lastDirection condition prevents the snake from doing a 180deg turn
 const controls = (event) => {
-  if (event.key === "ArrowUp") {
+  if (event.key === "ArrowUp" && lastDirection.x !== 0) {
     inputDirection = {x: 0, y: -1}
   }
 
-  else if (event.key === "ArrowDown") {
+  else if (event.key === "ArrowDown"  && lastDirection.x !== 0) {
     inputDirection = {x: 0, y: 1}
   }
 
-  else if (event.key === "ArrowLeft") {
+  else if (event.key === "ArrowLeft"  && lastDirection.y !== 0) {
     inputDirection = {x: -1, y: 0}
   }
 
-  else if (event.key === "ArrowRight") {
+  else if (event.key === "ArrowRight" && lastDirection.y !== 0) {
     inputDirection = {x: 1, y: 0}
   }
 }
@@ -27,5 +29,6 @@ window.addEventListener("keydown", controls)
 
 // gettter function for the input direction
 const getInputDirection = () => {
+  lastDirection = inputDirection
   return inputDirection
 }
